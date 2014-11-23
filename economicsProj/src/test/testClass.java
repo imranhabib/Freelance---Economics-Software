201b8faed0898d138a6400a8b24cfc777447843c
imranhabib <imranhabib14@hotmail.com>
2014-11-22 16:16:15 -0800

    m(xbar) equation

--- economicsProj/src/test/testClass.java
+++ economicsProj/src/test/testClass.java
@@ -1,9 +1,11 @@
 package test;
 
+import project.incomeRequired;
 import project.parameters;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Created by Imran on 2014-11-22.
@@ -11,11 +13,15 @@ import java.util.List;
 public class testClass {
 
     static parameters params;
+    static incomeRequired income;
 
    public static void main (String[] args) throws Exception{
        params = new parameters();
        int price = params.getMeanPriceAmount();
-       System.out.println(price);
+       int amount = params.getNumberOfSecurities();
+       income = new incomeRequired(2);
+       int mbar = income.getMbar(price,amount);
+       System.out.println(mbar);
 
 
 
