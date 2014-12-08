f07154238959fc8cf695bd419871bc810d170856
Imran Habib <imranhabib14@hotmail.com>
2014-12-08 13:05:48 -0800

    new ish

--- economicsProj/src/test/testClass.java
+++ economicsProj/src/test/testClass.java
@@ -1,6 +1,9 @@
 package test;
 
-import project.*;
+import project.allocation;
+import project.incomeRequired;
+import project.parameters;
+import project.sliderInterface;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,19 +17,58 @@ public class testClass {
     static incomeRequired income;
     static allocation allocation;
     static sliderInterface slider;
+    static int current;
+
+
+  public testClass(){
+     current = getCurrent();
+
+  }
+
+
+
+  public testClass(int current){
+    this.current = current;
+
+  }
+
+
+
+
+  public void incrementCurrent(){
+    current = current++;
+  }
+
+  public int getCurrent(){
+    return current;
+  }
+
+
+
+
 
    public static void main (String[] args) throws Exception {
        params = new parameters();
-       EventQueue.invokeLater(new Runnable() {
+
+     EventQueue.invokeLater(new Runnable() {
            public void run() {
+             int current = 1;
+               testClass test = new testClass(current);
+               test.incrementCurrent();
                sliderInterface frame = new sliderInterface(params.getSecurityList());
-
                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
                frame.setVisible(true);
            }
 
        });
+
+
+
+
+
+
+
+
    }
    }
 
