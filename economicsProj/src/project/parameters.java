94abbe85b80519d9fa9845d3cc7af5be5acbc782
mgwong <mmgwong@hotmail.com>
2014-12-08 15:00:16 -0800

    Update parameters.java
    fixed current

--- economicsProj/src/project/parameters.java
+++ economicsProj/src/project/parameters.java
@@ -1,10 +1,8 @@
 package project;
 
-import sun.text.IntHashtable;
+import test.testClass;
 
-import java.security.Security;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.ResourceBundle;
 
@@ -17,30 +15,33 @@ import java.util.ResourceBundle;
 
 public class parameters {
 
-    ResourceBundle bundle;
+  ResourceBundle bundle;
+  testClass testClass;
 
   public parameters(){
-      bundle = ResourceBundle.getBundle("resources/systemdata");
-
+    bundle = ResourceBundle.getBundle("resources/systemdata");
   }
 
-    public int getAllocation (){
-        return Integer.parseInt(bundle.getString("incomeHave"));
-    }
+  public int getAllocation (){
+    testClass test = new testClass();
+    return Integer.parseInt(bundle.getString("incomeHave" + test.getCurrent()));
+  }
 
-    public int getNumberOfSecurities(){
-        return Integer.parseInt(bundle.getString("securityAmount"));
-    }
+  public int getNumberOfSecurities(){
+    testClass test = new testClass();
+    return Integer.parseInt(bundle.getString("securityAmount" + test.getCurrent()));
+  }
 
-    public ArrayList<Integer> getSecurityPriceList(){
-        String price = bundle.getString("securityPriceList");
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+  public ArrayList<Integer> getSecurityPriceList(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceList" + test.getCurrent());
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
     /*
     public ArrayList<Float> getIncomeShareList(){
@@ -56,41 +57,44 @@ public class parameters {
 
     */
 
-    public List<Share> getSecurityList(){
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityList");
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceList();
-        //get income share
-     //   ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-        //    float is = incomeShareList.get(i-1);
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, i);
-            shares.add(share);
-        }
-        return shares;
-
+  public List<Share> getSecurityList(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    System.out.println("this is current = " + test.getCurrent());
+    String security = bundle.getString("securityList" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceList();
+    //get income share
+    //   ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      //    float is = incomeShareList.get(i-1);
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
 
-    public List<Share> getSecurityListWithEmptyIncomeShares(){
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityList");
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceList();
-        //get income share
-      //  ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
-
+  public List<Share> getSecurityListWithEmptyIncomeShares(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityList" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceList();
+    //get income share
+    //  ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
 
@@ -103,63 +107,64 @@ public class parameters {
 
 
 
-    // this calculates p(bar)
-    public double getMeanPriceAmount(){
-        String price = bundle.getString("securityPriceList");
-        String[] priceList = price.split(",");
-        List<Double> prices = new ArrayList<Double>();
-        for(String str: priceList){
-            prices.add(Double.parseDouble(str));
-        }
-        double sum = 0;
-        int amount = prices.size();
-        for(Double num: prices){
-            sum = sum + num;
-        }
-        return sum/amount;
-
-
+  // this calculates p(bar)
+  public double getMeanPriceAmount(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceList" + test.getCurrent());
+    String[] priceList = price.split(",");
+    List<Double> prices = new ArrayList<Double>();
+    for(String str: priceList){
+      prices.add(Double.parseDouble(str));
     }
-
-
-
-    //n(r) arg max pi/p1 <= r
-    public Double getArgMaxFormula(double r, List<Share> securities){
-        double argMax = 0;
-        int p1 = securities.get(0).getPrice();
-        double pi = 0;
-        double ratio = 0;
-        for(int i = 0; i < securities.size(); i++){
-            pi = securities.get(i).getPrice();
-            ratio = pi/p1;
-            if(ratio > r){
-                break;
-            }
-            argMax = (securities.get(i).getSecurityNumber());
-        }
-        return argMax;
+    double sum = 0;
+    int amount = prices.size();
+    for(Double num: prices){
+      sum = sum + num;
     }
+    return sum/amount;
 
-    //this calculates a(r)
-    public Double getAR(double r, List<Share> shares){
-        double nR = getArgMaxFormula(r, shares);
-        double sum = 0;
-        for(int i=1; i < nR+1; i++){
-            sum = sum + shares.get(i - 1).getIncomeShare();
-        }
-        double mult = (1/nR);
-        return (mult * sum) / 100;
-    }
 
-    //this calculates p(r)
-    public Double getPR(double r, List<Share> shares){
-        double nR = getArgMaxFormula(r, shares);
-        double sum = 0;
-        for(int i=1; i < nR+1; i++){
-            sum = sum + shares.get(i-1).getPrice();
-        }
-        return ((1/nR)* sum);
+  }
+
+
+
+  //n(r) arg max pi/p1 <= r
+  public Double getArgMaxFormula(double r, List<Share> securities){
+    double argMax = 0;
+    int p1 = securities.get(0).getPrice();
+    double pi = 0;
+    double ratio = 0;
+    for(int i = 0; i < securities.size(); i++){
+      pi = securities.get(i).getPrice();
+      ratio = pi/p1;
+      if(ratio > r){
+        break;
+      }
+      argMax = (securities.get(i).getSecurityNumber());
     }
+    return argMax;
+  }
+
+  //this calculates a(r)
+  public Double getAR(double r, List<Share> shares){
+    double nR = getArgMaxFormula(r, shares);
+    double sum = 0;
+    for(int i=1; i < nR+1; i++){
+      sum = sum + shares.get(i - 1).getIncomeShare();
+    }
+    double mult = (1/nR);
+    return (mult * sum) / 100;
+  }
+
+  //this calculates p(r)
+  public Double getPR(double r, List<Share> shares){
+    double nR = getArgMaxFormula(r, shares);
+    double sum = 0;
+    for(int i=1; i < nR+1; i++){
+      sum = sum + shares.get(i-1).getPrice();
+    }
+    return ((1/nR)* sum);
+  }
 
 
 
