7f7b1675c5ca791e53f3c764f3e4e0cd72f0d811
Imran Habib <imranhabib14@hotmail.com>
2014-12-17 17:49:50 -0800

    Update phase3.java

--- economicsProj/src/project/phase3.java
+++ economicsProj/src/project/phase3.java
@@ -1,9 +1,14 @@
 package project;
 
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.SoftBevelBorder;
+import java.awt.*;
+
 /**
  * Created by ihabib on 12/17/2014.
  */
-public class phase3 {
+public class phase3 extends JFrame{
 
 //TODO: Plan: phase 3 is going to access the data stored in excel files from stage 1, by using the code we already have.
   //TODO: cont. going to turn that code into one half of the allocation page. The other half will be passed from stage 2
@@ -13,6 +18,139 @@ public class phase3 {
 
 
 
+  static JFrame frameForPrices;
+
+
+  static JButton button1;
+  static JButton button2;
+  static JButton button3;
+  static JButton button4;
+  static JButton button5;
+  static JButton button6;
+  static JButton button7;
+  static JButton button8;
+  static JButton button9;
+  static JButton button10;
+  static JButton button11;
+  static JButton button12;
+  static JButton button13;
+  static JButton button14;
+
+  static JButton previewButton;
+
+  static JPanel buttonPanel;
+  static JPanel mainButtonsPanel;
+  static JPanel topTitlePanel;
+
+
+
+  static JLabel empty1;
+  static JLabel empty2;
+
+  static JLabel label1;
+  static JLabel label2;
+  static JLabel label3;
+
+
+  static JTextField jtext;
+
+
+
+
+  static Border raisedBorder;
+  static Border loweredBorder;
+
+
+
+
+  public phase3 () {
+
+
+    setLayout(new BorderLayout());
+    setTitle("Stage 3");
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    setBounds(0, 0, screenSize.width, screenSize.height - 45);
+
+    raisedBorder = new SoftBevelBorder(SoftBevelBorder.RAISED);
+    loweredBorder = new SoftBevelBorder(SoftBevelBorder.LOWERED);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  }
 
 
 
