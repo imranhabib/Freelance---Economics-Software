cad3cb154ac2aa75000f42dbeb1baa00ec22b691
Imran Habib <imranhabib14@hotmail.com>
2014-12-17 17:11:45 -0800

    Create phase3.java

---
+++ economicsProj/src/project/phase3.java
@@ -0,0 +1,105 @@
+package project;
+
+/**
+ * Created by ihabib on 12/17/2014.
+ */
+public class phase3 {
+
+//TODO: Plan: phase 3 is going to access the data stored in excel files from stage 1, by using the code we already have.
+  //TODO: cont. going to turn that code into one half of the allocation page. The other half will be passed from stage 2
+  //TODO: cont. Pressing buttons on the main stage3 phase will launch an allocation page comparing the data sets against
+  //TODO: cont. each other. 2 buttons on the bottom asking the user if they want to switch or stay
+  //TODO: potentially, show the user what there allocations would be with new prices? (code this option)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
