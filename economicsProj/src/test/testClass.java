8fd4418292804330e40353894691ae70c6123c9f
Imran Habib <imranhabib14@hotmail.com>
2014-12-21 20:48:40 -0800

    Update testClass.java

--- economicsProj/src/test/testClass.java
+++ economicsProj/src/test/testClass.java
@@ -1,9 +1,6 @@
 package test;
 
-import project.allocation;
-import project.incomeRequired;
-import project.parameters;
-import project.sliderInterface;
+import project.*;
 
 import javax.swing.*;
 import java.awt.*;
@@ -13,60 +10,63 @@ import java.awt.*;
  */
 public class testClass {
 
-  static parameters params;
-  static incomeRequired income;
-  static allocation allocation;
-  static sliderInterface slider;
-  static int current;
+    static parameters params;
+    static incomeRequired income;
+    static allocation allocation;
+    static sliderInterface slider;
+    static int current;
 
 
-  public testClass(){
-    current = getCurrent();
-  }
-
-
-
-  public testClass(int current){
-    this.current = current;
-
-  }
+    public testClass(){
+        current = getCurrent();
+    }
 
 
 
+    public testClass(int current){
+        this.current = current;
 
-  public void incrementCurrent(){
-    current = current++;
-  }
-
-  public int getCurrent(){
-    return current;
-  }
+    }
 
 
 
 
 
-  public static void main (String[] args) throws Exception {
-    params = new parameters();
+    public void incrementCurrent(){
+        current = current++;
+    }
 
-    EventQueue.invokeLater(new Runnable() {
-      public void run() {
-        testClass test = new testClass(1);
-        sliderInterface frame = new sliderInterface(params.getSecurityList(), test.getCurrent());
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-      }
-
-    });
+    public int getCurrent(){
+        return current;
+    }
 
 
 
 
 
+    public static void main (String[] args) throws Exception {
+        params = new parameters();
+
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                testClass test = new testClass(1);
+                //uncomment slider to run from stage2 and uncomment phase1 to run from phase1
+                sliderInterface frame = new sliderInterface(params.getSecurityList(), test.getCurrent(), false, false, false, false,  0, 0.0);
+                //phase1 frame = new phase1(params.getSecurityList(), test.getCurrent());
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+
+        });
 
 
 
-  }
+
+
+
+
+
+    }
 }
 
 
