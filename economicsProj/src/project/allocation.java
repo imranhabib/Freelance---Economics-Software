fe2c9d0d0208f0a9f0ea006a6e62103ff9be22fc
Imran Habib <imranhabib14@hotmail.com>
2015-02-27 13:09:11 -0800

    Update allocation.java

--- economicsProj/src/project/allocation.java
+++ economicsProj/src/project/allocation.java
@@ -8,60 +8,61 @@ import java.util.List;
  */
 public class allocation {
 
-  static parameters params;
-  static incomeRequired income;
+    static parameters params;
+    static incomeRequired income;
 
-  public allocation(parameters parameter, incomeRequired incomerequired) {
-    income = incomerequired;
-    params = parameter;
-  }
-
-  public Double allocationForShare(int shareNumber, double r, List<Share> shares, int x, String priceSet) {
-    int m = params.getAllocation();
-    Double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
-    if (m < mXBar) {
-      return allocationMethodOne(shares, priceSet);
+    public allocation(parameters parameter, incomeRequired incomerequired) {
+        income = incomerequired;
+        params = parameter;
     }
-    double nR = params.getArgMaxFormula(r, shares);
-    if (shareNumber <= nR) {
-      return allocationMethodTwo(r, shares, x, priceSet);
-    } else
-      return allocationMethodThree(shareNumber, shares, x, priceSet);
-  }
 
-  public Double allocationMethodOne(List<Share> shares, String priceSet) {
-    int m = params.getAllocation();
-    Double p = params.getMeanPriceAmount(priceSet);
-    int n = shares.size();
+    public Double allocationForShare(int shareNumber, double r, List<Share> shares, int x, String priceSet) {
+        int m = params.getAllocation();
+        Double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
+        if (m < mXBar) {
+            return allocationMethodOne(shares, priceSet);
+        }
+        double nR = params.getArgMaxFormula(r, shares);
+        if (shareNumber <= nR) {
+            return allocationMethodTwo(r, shares, x, priceSet);
+        } else
+            return allocationMethodThree(shareNumber, shares, x, priceSet);
+    }
 
-    return m / (n * p);
-  }
+    public Double allocationMethodOne(List<Share> shares, String priceSet) {
+        int m = params.getAllocation();
+        Double p = params.getMeanPriceAmount(priceSet);
+        int n = shares.size();
 
-
-  public Double allocationMethodTwo(double r, List<Share> shares, int x, String priceSet) {
-    int m = params.getAllocation();
-    double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
-    double aR = params.getAR(r, shares);
-    double pR = params.getPR(r, shares);
-
-    return x + ((aR * (m - mXBar)) / pR);
-  }
-
-  public Double allocationMethodThree(int shareNumber, List<Share> shares, int x, String priceSet) {
-    System.out.println("shareNumber = " + shareNumber);
-    int m = params.getAllocation();
-    Double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
-    Double aI = (shares.get(shareNumber - 1).getIncomeShare()) / 100;
-    double pI = (shares.get(shareNumber - 1).getPrice())/10;
-    System.out.println("here is pI "  + pI);
+        return m / (n * p);
+    }
 
 
+    public Double allocationMethodTwo(double r, List<Share> shares, int x, String priceSet) {
+        int m = params.getAllocation();
+        double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
+        double aR = params.getAR(r, shares);
+        double pR = params.getPR(r, shares);
+
+        return x + ((aR * (m - mXBar)) / pR);
+    }
+
+    public Double allocationMethodThree(int shareNumber, List<Share> shares, int x, String priceSet) {
+        System.out.println("shareNumber = " + shareNumber);
+        int m = params.getAllocation();
+        Double mXBar = income.getMbar(params.getMeanPriceAmount(priceSet), shares.size());
+        System.out.println("here is ghghgh "  + (shares.get(shareNumber - 1).getIncomeShare()) / 100);
+        Double aI = (shares.get(shareNumber - 1).getIncomeShare()) / 100;
+        double pI = (shares.get(shareNumber - 1).getPrice())/10;
+        System.out.println("here is pI "  + pI);
 
 
-    return x + ((aI * (m - mXBar)) / pI);
 
 
-  }
+        return x + ((aI * (m - mXBar)) / pI);
+
+
+    }
 
 
 }
