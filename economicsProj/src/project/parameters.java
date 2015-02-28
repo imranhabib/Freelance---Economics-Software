4847b5cc8cbc77415b24c505a2ce2a17d3094a8c
Imran Habib <imranhabib14@hotmail.com>
2015-02-27 16:45:46 -0800

    Update parameters.java

--- economicsProj/src/project/parameters.java
+++ economicsProj/src/project/parameters.java
@@ -15,295 +15,295 @@ import java.util.ResourceBundle;
 
 public class parameters {
 
-    ResourceBundle bundle;
-    testClass testClass;
+  ResourceBundle bundle;
+  testClass testClass;
 
-    public parameters(){
-        bundle = ResourceBundle.getBundle("resources/systemdata");
+  public parameters(){
+    bundle = ResourceBundle.getBundle("resources/systemdata");
+  }
+
+  public int getAllocation (){
+    testClass test = new testClass();
+    return Integer.parseInt(bundle.getString("incomeHave" + test.getCurrent()));
+  }
+
+  public int getNumberOfSecurities(){
+    testClass test = new testClass();
+    return Integer.parseInt(bundle.getString("securityAmount" + test.getCurrent()));
+  }
+
+  public ArrayList<Integer> getSecurityPriceList(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceList" + test.getCurrent());
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
-    public int getAllocation (){
-        testClass test = new testClass();
-        return Integer.parseInt(bundle.getString("incomeHave" + test.getCurrent()));
+
+
+
+  public ArrayList<Integer> getSecurityPriceListStage2(){
+    testClass test = new testClass();
+    String price = bundle.getString("priceSet" + test.getCurrent());
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
-    public int getNumberOfSecurities(){
-        testClass test = new testClass();
-        return Integer.parseInt(bundle.getString("securityAmount" + test.getCurrent()));
+
+
+  public ArrayList<Integer> getSecurityPriceListStage2Test(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceListStage2Test" + test.getCurrent());
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
-    public ArrayList<Integer> getSecurityPriceList(){
-        testClass test = new testClass();
-        String price = bundle.getString("securityPriceList" + test.getCurrent());
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+
+  public ArrayList<Integer> getSecurityPriceListStage1TestRounds(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceListTest" + test.getCurrent());
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
 
 
-
-    public ArrayList<Integer> getSecurityPriceListStage2(){
-        testClass test = new testClass();
-        String price = bundle.getString("priceSet" + test.getCurrent());
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+  public ArrayList<Integer> getSecurityPriceListFromStart(){
+    testClass test = new testClass();
+    String price = bundle.getString("securityPriceList" + 1);
+    String[] priceList = price.split(",");
+    ArrayList<Integer> prices = new ArrayList<Integer>();
+    for(String str: priceList){
+      prices.add(Integer.parseInt(str));
     }
+    return prices;
+  }
 
 
 
-    public ArrayList<Integer> getSecurityPriceListStage2Test(){
-        testClass test = new testClass();
-        String price = bundle.getString("securityPriceListStage2Test" + test.getCurrent());
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+
+  public List<Share> getSecurityList(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    System.out.println("this is current = " + test.getCurrent());
+    String security = bundle.getString("securityList" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceList();
+    //get income share
+    //   ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      //    float is = incomeShareList.get(i-1);
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
-    public ArrayList<Integer> getSecurityPriceListStage1TestRounds(){
-        testClass test = new testClass();
-        String price = bundle.getString("securityPriceListTest" + test.getCurrent());
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+
+  public List<Share> getSecurityListFromStart(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+
+    String security = bundle.getString("securityList" + 1);
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceListFromStart();
+    //get income share
+    //   ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      //    float is = incomeShareList.get(i-1);
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
-
-    public ArrayList<Integer> getSecurityPriceListFromStart(){
-        testClass test = new testClass();
-        String price = bundle.getString("securityPriceList" + 1);
-        String[] priceList = price.split(",");
-        ArrayList<Integer> prices = new ArrayList<Integer>();
-        for(String str: priceList){
-            prices.add(Integer.parseInt(str));
-        }
-        return prices;
+  public List<Share> getSecurityListWithEmptyIncomeSharesStage2test(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityList" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceListStage2Test();
+    //get income share
+    //  ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
 
+  }
 
-
-
-    public List<Share> getSecurityList(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        System.out.println("this is current = " + test.getCurrent());
-        String security = bundle.getString("securityList" + test.getCurrent());
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceList();
-        //get income share
-        //   ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            //    float is = incomeShareList.get(i-1);
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, i);
-            shares.add(share);
-        }
-        return shares;
-
+  public List<Share> getSecurityListWithEmptyIncomeShares(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityList" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceList();
+    //get income share
+    //  ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
-
-    public List<Share> getSecurityListFromStart(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-
-        String security = bundle.getString("securityList" + 1);
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceListFromStart();
-        //get income share
-        //   ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            //    float is = incomeShareList.get(i-1);
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, i);
-            shares.add(share);
-        }
-        return shares;
-
+  public List<Share> getSecurityListWithEmptyIncomeSharesStage2Prices(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityListTwo" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceListStage2();
+    //get income share
+    //  ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
-    public List<Share> getSecurityListWithEmptyIncomeSharesStage2test(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityList" + test.getCurrent());
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceListStage2Test();
-        //get income share
-        //  ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
+  public List<Share> getSecurityListWithEmptyIncomeSharesStage1TestRoundPrices(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityListTest" + test.getCurrent());
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceListStage1TestRounds();
 
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
 
-    public List<Share> getSecurityListWithEmptyIncomeShares(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityList" + test.getCurrent());
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceList();
-        //get income share
-        //  ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
+  }
 
+
+
+  public List<Share> getSecurityListWithEmptyIncomeSharesFromStart(){
+    testClass test = new testClass();
+    ArrayList<Share> shares = new ArrayList<Share>();
+    String security = bundle.getString("securityList" + 1);
+    String[] securityNumber = security.split(",");
+    ArrayList<Integer> securityPrices = getSecurityPriceListFromStart();
+    //get income share
+    //  ArrayList<Float> incomeShareList = getIncomeShareList();
+    for (int i=1; i < securityNumber.length+1; i++) {
+      Double is = 0.0;
+      int sp = securityPrices.get(i-1);
+      Share share = new Share(sp, is, i);
+      shares.add(share);
     }
+    return shares;
+
+  }
 
 
-    public List<Share> getSecurityListWithEmptyIncomeSharesStage2Prices(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityListTwo" + test.getCurrent());
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceListStage2();
-        //get income share
-        //  ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
 
+
+
+
+
+
+
+
+
+
+
+  // this calculates p(bar)
+  public double getMeanPriceAmount(String priceSet){
+    testClass test = new testClass();
+    String price = bundle.getString(priceSet + test.getCurrent());
+    String[] priceList = price.split(",");
+    List<Double> prices = new ArrayList<Double>();
+    for(String str: priceList){
+      prices.add(Double.parseDouble(str));
+      System.out.println("here are prices doe " + str);
     }
-
-
-    public List<Share> getSecurityListWithEmptyIncomeSharesStage1TestRoundPrices(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityListTest" + test.getCurrent());
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceListStage1TestRounds();
-
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
-
+    double sum = 0;
+    int amount = prices.size();
+    for(Double num: prices){
+      sum = sum + num;
     }
+    System.out.println("sum " + (sum/amount)/100);
+    return (sum/amount)/100;
+
+
+  }
 
 
 
-    public List<Share> getSecurityListWithEmptyIncomeSharesFromStart(){
-        testClass test = new testClass();
-        ArrayList<Share> shares = new ArrayList<Share>();
-        String security = bundle.getString("securityList" + 1);
-        String[] securityNumber = security.split(",");
-        ArrayList<Integer> securityPrices = getSecurityPriceListFromStart();
-        //get income share
-        //  ArrayList<Float> incomeShareList = getIncomeShareList();
-        for (int i=1; i < securityNumber.length+1; i++) {
-            Double is = 0.0;
-            int sp = securityPrices.get(i-1);
-            Share share = new Share(sp, is, i);
-            shares.add(share);
-        }
-        return shares;
-
+  //n(r) arg max pi/p1 <= r
+  public Double getArgMaxFormula(double r, List<Share> securities){
+    double argMax = 0;
+    double p1 = (securities.get(0).getPrice())/100;
+    double pi = 0;
+    double ratio = 0;
+    for(int i = 0; i < securities.size(); i++){
+      pi = (securities.get(i).getPrice());
+      ratio = pi/p1;
+      if(ratio > r){
+        break;
+      }
+      argMax = (securities.get(i).getSecurityNumber());
     }
+    return argMax;
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-    // this calculates p(bar)
-    public double getMeanPriceAmount(String priceSet){
-        testClass test = new testClass();
-        String price = bundle.getString(priceSet + test.getCurrent());
-        String[] priceList = price.split(",");
-        List<Double> prices = new ArrayList<Double>();
-        for(String str: priceList){
-            prices.add(Double.parseDouble(str));
-            System.out.println("here are prices doe " + str);
-        }
-        double sum = 0;
-        int amount = prices.size();
-        for(Double num: prices){
-            sum = sum + num;
-        }
-        System.out.println("sum " + (sum/amount)/100);
-        return (sum/amount)/100;
-
-
+  //this calculates a(r)
+  public Double getAR(double r, List<Share> shares){
+    double nR = getArgMaxFormula(r, shares);
+    double sum = 0;
+    for(int i=1; i < nR+1; i++){
+      sum = sum + shares.get(i - 1).getIncomeShare();
     }
+    double mult = (1/nR);
+    return (mult * sum) / 100;
+  }
 
-
-
-    //n(r) arg max pi/p1 <= r
-    public Double getArgMaxFormula(double r, List<Share> securities){
-        double argMax = 0;
-        double p1 = (securities.get(0).getPrice())/100;
-        double pi = 0;
-        double ratio = 0;
-        for(int i = 0; i < securities.size(); i++){
-            pi = (securities.get(i).getPrice());
-            ratio = pi/p1;
-            if(ratio > r){
-                break;
-            }
-            argMax = (securities.get(i).getSecurityNumber());
-        }
-        return argMax;
+  //this calculates p(r)
+  public Double getPR(double r, List<Share> shares){
+    double nR = getArgMaxFormula(r, shares);
+    double sum = 0;
+    for(int i=1; i < nR+1; i++){
+      sum = sum + ((shares.get(i-1).getPrice())/100);
     }
-
-    //this calculates a(r)
-    public Double getAR(double r, List<Share> shares){
-        double nR = getArgMaxFormula(r, shares);
-        double sum = 0;
-        for(int i=1; i < nR+1; i++){
-            sum = sum + shares.get(i - 1).getIncomeShare();
-        }
-        double mult = (1/nR);
-        return (mult * sum) / 100;
-    }
-
-    //this calculates p(r)
-    public Double getPR(double r, List<Share> shares){
-        double nR = getArgMaxFormula(r, shares);
-        double sum = 0;
-        for(int i=1; i < nR+1; i++){
-            sum = sum + ((shares.get(i-1).getPrice())/100);
-        }
-        return ((1/nR)* sum);
-    }
+    return ((1/nR)* sum);
+  }
 
 
 
