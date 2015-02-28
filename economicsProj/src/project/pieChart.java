290498017c51e1bed6f8d9d606d9e35657860c74
Imran Habib <imranhabib14@hotmail.com>
2015-02-27 16:47:47 -0800

    Update pieChart.java

--- economicsProj/src/project/pieChart.java
+++ economicsProj/src/project/pieChart.java
@@ -7,9 +7,7 @@ import org.jfree.chart.labels.PieSectionLabelGenerator;
 import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
 import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
 
-import java.text.DecimalFormat;
 import java.util.List;
 
 
@@ -18,42 +16,42 @@ import java.util.List;
  */
 public class pieChart {
 
-    List<Share> shareListFordata;
-    DefaultPieDataset data;
-    JFreeChart pieChart;
+  List<Share> shareListFordata;
+  DefaultPieDataset data;
+  JFreeChart pieChart;
 
 
-    public pieChart(List<Share> shareList){
-        shareListFordata = shareList;
-        data = new DefaultPieDataset();
+  public pieChart(List<Share> shareList){
+    shareListFordata = shareList;
+    data = new DefaultPieDataset();
 
-        for(int i = 0; i < shareListFordata.size(); i++){
-            double allocation = shareListFordata.get(i).getAllocation();
-            String name = Integer.toString(shareListFordata.get(i).getSecurityNumber());
-            String build = "Security " + name;
-            data.setValue(build, allocation);
-
-
-        }
-
-        pieChart = ChartFactory.createPieChart("Allocations", data, true, true, false);
-        PiePlot plot = (PiePlot) pieChart.getPlot();
-
-
-
-
-        PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
-                "{0}: {1} ({2})", new DecimalFormat("0.0"), new DecimalFormat("0%"));
-        plot.setLabelGenerator(labelGenerator);
-
+    for(int i = 0; i < shareListFordata.size(); i++){
+      double allocation = shareListFordata.get(i).getAllocation();
+      String name = Integer.toString(shareListFordata.get(i).getSecurityNumber());
+      String build = "Account " + name;
+      data.setValue(build, allocation);
 
 
     }
 
+    pieChart = ChartFactory.createPieChart("Allocations", data, true, true, false);
+    PiePlot plot = (PiePlot) pieChart.getPlot();
 
-    public JFreeChart getChart(){
-        return pieChart;
 
-    }
+
+
+    PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
+        "{0}: ({2})");
+    plot.setLabelGenerator(labelGenerator);
+
+
+
+  }
+
+
+  public JFreeChart getChart(){
+    return pieChart;
+
+  }
 
 }
