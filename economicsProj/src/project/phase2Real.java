7fb3a46c52a7fa128b15874954fe943408f1cf8b
Imran Habib <imranhabib14@hotmail.com>
2015-03-07 09:10:29 -0800

    Update phase2Real.java

--- economicsProj/src/project/phase2Real.java
+++ economicsProj/src/project/phase2Real.java
@@ -890,7 +890,7 @@ public class phase2Real extends JFrame {
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new GridLayout(1, 2, 5, 5));
 
-        final JButton button1 = new JButton("View token allocations as a chart");
+        final JButton button1 = new JButton("Minimum number of tokens in each account");
         button1.setLayout(new GridLayout(0, 3));
         button1.setBorder(new TitledBorder("Chart"));
 
